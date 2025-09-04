e9e844537e0f10dce0066a06a1645322e2006d9e
kashok <ashok.7406625107@gmail.com>
2025-09-04 11:09:26 +0530

    commiting and pushing again for lab 4 program

---
+++ src/BasicPrograms/_Methods.java
@@ -0,0 +1,15 @@
+package BasicPrograms;
+
+public class _Methods {
+
+    static void add(){
+        int a = 10;
+        int b=20;
+        int c=a+b;
+        System.out.println("The addition of 2 numbers is"+c);
+    }
+    public static void main(String[] args){
+        add();
+
+    }
+}
