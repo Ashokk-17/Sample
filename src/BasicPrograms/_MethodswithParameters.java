a92a00d63b6ac851c1aa9d75ee4a8aae03c921fd
kashok <ashok.7406625107@gmail.com>
2025-09-04 11:17:56 +0530

    commiting for methods with parameters

---
+++ src/BasicPrograms/_MethodswithParameters.java
@@ -0,0 +1,14 @@
+package BasicPrograms;
+
+public class _MethodswithParameters {
+    static void add (int a, int b){
+        int x = a-b;
+        System.out.println("The subtraction of 2 numbers is "+x);
+    }
+
+    public static void main(String[] args) {
+        add(40,10);
+
+
+    }
+}
