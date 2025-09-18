211bc9c234b29ff8f2a797531fd40bfbd02b9bd5
Ashok K <ashok.7406625107@gmail.com>
2025-09-18 22:16:56 +0530

    Revert "comitting for lab 05"

--- src/ex_02_Java_Basic/lab_05.java
+++
@@ -1,9 +0,0 @@
-package ex_02_Java_Basic;
-
-import java.sql.SQLOutput;
-
-public class lab_05 {
-    public static void main(String[] args) {
-        System.out.println("This is Ashok using literals ");
-    }
-}
